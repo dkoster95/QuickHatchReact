baab495b2e960323bf450f4ae65aa677650f6083
Daniel Koster <danielkoster95@gmail.com>
2020-10-04 23:29:37 -0400

    first upload

---
+++ Sources/QuickHatchReact.h
@@ -0,0 +1,18 @@
+//
+//  QuickHatchReact.h
+//  QuickHatchReact
+//
+//  Created by Daniel Koster on 10/4/20.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for QuickHatchReact.
+FOUNDATION_EXPORT double QuickHatchReactVersionNumber;
+
+//! Project version string for QuickHatchReact.
+FOUNDATION_EXPORT const unsigned char QuickHatchReactVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <QuickHatchReact/PublicHeader.h>
+
+
